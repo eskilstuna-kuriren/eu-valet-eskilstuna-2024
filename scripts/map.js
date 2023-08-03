da10d8cfd324109615655bd8b3d69043e549b990
andreaslindstrom-ntm <141117488+andreaslindstrom-ntm@users.noreply.github.com>
2023-08-03 09:51:03 +0200

    Update map.js
    If url link is included in the "Link" field in Google sheets, add a "Read more here" hypertext in the popup.

--- scripts/map.js
+++ scripts/map.js
@@ -129,7 +129,8 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description']);
+          point['Description'] + '<br>' +
+          (point['Link'] ? ('<a href="' + point['Link'] + '" target="_blank">Read more here</a>') : ''));
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
